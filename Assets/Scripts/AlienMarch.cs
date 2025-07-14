9040d793597c579592c54cda3edd389313405fe5
Johnson Doong <156211561+JagerBigMan@users.noreply.github.com>
2025-07-13 23:51:13 -0400

    Updated Space Invader exercise (completed)
    I made the alien march down the screen in intervals, so it's not sliding down the screen. I used if (timer > stepInterval) to make it march down 1 unit every 0.5s

--- Assets/Scripts/AlienMarch.cs
+++ Assets/Scripts/AlienMarch.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class AlienMarch : MonoBehaviour
 {
-    public float moveSpeed = 1f;
+    public float stepDistance = 0.5f;
+    public float stepInterval = 1f;
+
+    private float timer = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,12 @@ public class AlienMarch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        timer += Time.deltaTime;
+        //adding the real world time to the timer
+        if(timer > stepInterval) //this helps to continuously march down the alien march down the moment 0.5s real world time has passed
+        {
+            transform.position += Vector3.down * stepDistance; //this then moves the alien down by 1 unit
+            timer = 0f;  //resetting the timer so that it can be run again
+        }
     }
 }
